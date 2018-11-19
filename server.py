163e0c01b6084b3bd420c2fed1dc32f06431c7d2
Julia Ortiz Naharro <jortizna@alpha09.aulas.gsyc.urjc.es>
2018-11-19 14:37:45 +0100

    Server bien

--- server.py
+++ server.py
@@ -1,49 +1,51 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Clase (y programa principal) para un servidor de eco en UDP simple
-"""
+"""Clase (y programa principal) para un servidor de eco en UDP simple."""
 
 import socketserver
 import sys
 import os
 
-if (len(sys.argv) == 4):
+if len(sys.argv) == 4:
     IP = sys.argv[1]
     PORT = int(sys.argv[2])
     fichero_audio = sys.argv[3]
-else:
+elif len(sys.argv) != 4:
     sys.exit("Usage: python3 server.py IP port audio_file")
+else:
+    self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
 
 class EchoHandler(socketserver.DatagramRequestHandler):
-    """
-    Echo server class
-    """
+    """Echo server class."""
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-    #print("Registro de clientes:")
+        """Filtrar por métodos."""
         for line in self.rfile:
             method = line.decode('utf-8').split(" ")[0]
-            if (method == "INVITE"):
-                self.wfile.write(b"SIP/2.0 100 Trying SIP/2.0 180 Ringing SIP/2.0 200 OK \r\n\r\n")
-                print("El cliente nos manda " + line.decode('utf-8'))
+            try:
+                if method == "INVITE":
+                    self.wfile.write(b"SIP/2.0 100 Trying SIP/2.0 180 Ringing SIP/2.0 200 OK \r\n\r\n")
+                    print("El cliente nos manda " + line.decode('utf-8'))
 
-            elif (method == "ACK"):
-                print("El cliente nos manda " + line.decode('utf-8'))
-                aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + fichero_audio
-                print("Vamos a ejecutar", aEjecutar)
-                os.system(aEjecutar)
-                print("Cancion enviada")
+                elif method == "ACK":
+                    print("El cliente nos manda " + line.decode('utf-8'))
+                    aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + fichero_audio
+                    print("Vamos a ejecutar", aEjecutar)
+                    os.system(aEjecutar)
+                    print("Cancion enviada")
 
-            elif (method == "BYE"):
-                print("El cliente nos manda " + line.decode('utf-8'))
-                self.wfile.write(b"SIP/2.0 200 OK \r\n\r\n")
+                elif method == "BYE":
+                    print("El cliente nos manda " + line.decode('utf-8'))
+                    self.wfile.write(b"SIP/2.0 200 OK \r\n\r\n")
 
-            #elif (method == "BYE"):
+            except method != ("INVITE" or "ACK" or "BYE"):
+                self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = socketserver.UDPServer((IP, PORT), EchoHandler)
     print("Listening...")
-    serv.serve_forever()
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("Finalizado servidor")

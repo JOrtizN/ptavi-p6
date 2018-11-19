41f371969118f518360f90cf3363b195d0f05823
Julia Ortiz Naharro <jortizna@gamma15.aulas.gsyc.urjc.es>
2018-11-19 09:45:17 +0100

    manda ack para trying, forzado el connect

--- client.py
+++ client.py
@@ -5,26 +5,36 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
-# Cliente UDP simple.
-
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
-
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
-
+METHOD = ""
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
 
-    print("Enviando: " + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    if len(sys.argv) == 3:
+        METHOD = sys.argv[1]
+        USER = sys.argv[2].split("@")[0]
+        IP = sys.argv[2].split("@")[1].split(":")[0]
+        PORT = sys.argv[2].split("@")[1].split(":")[1]
+
+        #  my_socket.connect((IP, PORT))
+        my_socket.connect(('localhost', 6001))
+        my_socket.send(bytes(METHOD + " sip:" + USER + "@" + IP +
+                        " SIP/2.0", 'utf-8') + b'\r\n\r\n')
+    else:
+        print("Usage: python3 client.py method receiver@IP:SIPport")
+
+
     data = my_socket.recv(1024)
+    Number_data = data.decode('utf-8').split(" ")[1]
+    print(Number_data)
 
-    print('Recibido -- ', data.decode('utf-8'))
+    if Number_data == "100":
+        my_socket.send(bytes("ACK" + " sip:" + USER + "@" + IP +
+                        " SIP/2.0", 'utf-8') + b'\r\n\r\n')
+    else:
+        print('Recibido -- ', data.decode('utf-8'))
     print("Terminando socket...")
 
 print("Fin.")

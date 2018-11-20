7df18425069bce181a6a6cb5b12fd82cd0c41363
Julia Ortiz Naharro <jortizna@alpha03.aulas.gsyc.urjc.es>
2018-11-20 14:28:36 +0100

    Cliente con todos los errores corregidos y pasados los 2 pep

--- client.py
+++ client.py
@@ -12,25 +12,34 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
 
     if len(sys.argv) == 3:
         METHOD = sys.argv[1]
+    else:
+        sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
+    try:
         USER = sys.argv[2].split("@")[0]
         IP = sys.argv[2].split("@")[1].split(":")[0]
         PORT = int(sys.argv[2].split("@")[1].split(":")[1])
-
         my_socket.connect((IP, PORT))
         my_socket.send(bytes(METHOD + " sip:" + USER + "@" + IP +
-                       " SIP/2.0", 'utf-8') + b'\r\n\r\n')
-    else:
-        print("Usage: python3 client.py method receiver@IP:SIPport")
+                             " SIP/2.0", 'utf-8') + b'\r\n\r\n')
+    except IndexError:
+        sys.exit("Usage: Error in -> receiver@IP:SIPport <-")
 
     data = my_socket.recv(1024)
     n_data = data.decode('utf-8').split()
+    print(data.decode('utf-8'))
 
     if n_data[1] == "100" and n_data[4] == "180" and n_data[7] == "200":
-        print("Send ACK")
+        print("Send ACK, if you have to wait your request is okey")
         my_socket.send(bytes("ACK" + " sip:" + USER + "@" + IP +
                        " SIP/2.0", 'utf-8') + b'\r\n\r\n')
-    else:
-        print('Recibido -- ', data.decode('utf-8'))
+        data = my_socket.recv(1024)
+        n_data = data.decode('utf-8').split()
+        try:
+            if n_data[1] == "400" or n_data[1] == "405":
+                print(data.decode('utf-8'))
+        except IndexError:
+            pass
+
     print("Terminando socket...")
 
 print("Fin.")
